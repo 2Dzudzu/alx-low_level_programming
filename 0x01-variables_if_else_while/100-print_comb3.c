d6aadce120caaf26e5ab663c8b0ce121e190e805
2Dzudzu <petroszulu4@gmail.com>
2023-03-05 11:33:38 -0800

    by two

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,29 +2,28 @@
 
 /**
  * main - prints all possible different combinations of two digits
- *
- * Return: Always 0.
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int digit1, digit2;
+	int n, m;
 
-	for (digit1 = 48; digit1 <= 56; digit1++)
+	for (n = 48; n <= 56; n++)
 	{
-		for (digit2 = 49; digit2 <= 57; digit2++)
+		for (m = 49; m <= 57; m++)
 		{
-			putchar((digit1 % 10) + '0');
-			putchar((digit2 % 10) + '0');
-
-			if (digit1 == 56 && digit2 == 57)
-				continue;
-
-			putchar(',');
-			putchar(' ');
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
