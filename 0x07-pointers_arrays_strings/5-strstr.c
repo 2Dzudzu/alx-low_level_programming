6f97a26e120b774fa47339aa99f38ac783f443d8
2Dzudzu <petroszulu4@gmail.com>
2023-03-06 12:26:01 -0800

    0x07. C - Even more pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *_strstr - locates a substring within a string
+ *
+ *@haystack: pointer that contains the address of the string to be evaluated
+ *@needle: pointer that contains the string to evaluate @haystack
+ *
+ *Return: address of the located string in @haystack if no substring is found
+ *then retun NULL
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	char *result = haystack, *fneedle = needle;
+
+	while (*haystack)
+	{
+		while (*needle)
+		{
+			if (*haystack++ != *needle++)
+			{
+				break;
+			}
+		}
+		if (!*needle)
+		{
+			return (result);
+		}
+		needle = fneedle;
+		result++;
+		haystack = result;
+	}
+	return (0);
+}
