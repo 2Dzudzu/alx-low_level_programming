d72efcf2be5b5bdd82020daeef6b3c5a445929cb
2Dzudzu <petroszulu4@gmail.com>
2023-04-11 10:49:39 -0700

    quick fix

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,38 +1,37 @@
 #include "main.h"
 
 /**
- * binary_to_uint - converts a binary number to anunsigned int.
+ * binary_to_uint - converts a binary number to an
+ * unsigned int.
  * @b: binary.
+ *
  * Return: unsigned int.
  */
-
 unsigned int binary_to_uint(const char *b)
-
 {
-	unsigned int pos;
-	int rue;
-	int ken;
+	unsigned int ui;
+	int len, base_two;
 
 	if (!b)
 		return (0);
 
-	pos = 0;
+	ui = 0;
 
-	for (rue = 0; b[rue] != '\0'; rue++)
+	for (len = 0; b[len] != '\0'; len++)
 		;
 
-	for (rue--, ken = 1; rue >= 0; rue--, ken *= 2)
+	for (len--, base_two = 1; len >= 0; len--, base_two *= 2)
 	{
-		if (b[rue] != '0' && b[rue] != '1')
+		if (b[len] != '0' && b[len] != '1')
 		{
 			return (0);
 		}
 
-		if (b[rue] & 1)
+		if (b[len] & 1)
 		{
-			pos += ken;
+			ui += base_two;
 		}
 	}
 
-	return (pos);
+	return (ui);
 }
